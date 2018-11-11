3ba339faf94eb13d20118ec60a392932d2585fe2
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-11-11 09:57:50 -0600

    Adds problem 2 solution

---
+++ src/Problem2.hs
@@ -0,0 +1,10 @@
+module Probelm2 where
+
+sumEvenFibsLE :: (Integral a) => a -> a
+sumEvenFibsLE n = let sumFibHelper acc f0 f1 max
+                        | cur > max = acc
+                        | cur `mod` 2 == 0 = sumFibHelper (acc+cur) f1 cur max
+                        | otherwise = sumFibHelper acc f1 cur max
+                        where cur = f0 + f1
+                      in
+                  sumFibHelper 0 0 1 n
